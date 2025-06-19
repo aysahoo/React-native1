2de2763a745d29e2d97a923cd860007eab16f2be
Ayush Ranjan Sahoo <77970726+aysahoo@users.noreply.github.com>
2025-06-19 15:29:36 +0530

    Changed again

--- Bhishaj/app/(tabs)/explore.tsx
+++ Bhishaj/app/(tabs)/explore.tsx
@@ -21,7 +21,7 @@ export default function TabTwoScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Ankit Kumar</ThemedText>
+        <ThemedText type="title">Ankit Kumar Love</ThemedText>
       </ThemedView>
       <ThemedText>This app includes example code to help you get started.</ThemedText>
       <Collapsible title="File-based routing">
